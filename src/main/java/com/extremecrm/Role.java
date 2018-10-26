64f0a1edd872b5039a3074e5706f9ade26d048b9
HP <HP@192.168.1.101>
2018-10-26 23:27:37 +0530

    CRM From Sourabh

---
+++ src/main/java/com/extremecrm/Role.java
@@ -0,0 +1,33 @@
+package com.extremecrm;
+
+import java.util.List;
+
+public class Role {
+	
+	private String name;
+	private List<User> user;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<User> getUser() {
+		return user;
+	}
+	public void setUser(List<User> user) {
+		this.user = user;
+	}
+	public Role(String name, List<User> user) {
+		this.name = name;
+		this.user = user;
+	}
+	public Role() {
+	}
+	public Role(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/extremecrm/Task.java
@@ -0,0 +1,70 @@
+package com.extremecrm;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Task {
+	
+	private Long id;
+	private String data;
+	private String startTime;
+	private String StopTime;
+	private String descriptaion;
+	@ManyToOne
+	@JoinColumn(name="USER_EMAIL")
+	private User user;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getStopTime() {
+		return StopTime;
+	}
+	public void setStopTime(String stopTime) {
+		StopTime = stopTime;
+	}
+	public String getDescriptaion() {
+		return descriptaion;
+	}
+	public void setDescriptaion(String descriptaion) {
+		this.descriptaion = descriptaion;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Task(String data, String startTime, String stopTime, String descriptaion, User user) {
+		this.data = data;
+		this.startTime = startTime;
+		StopTime = stopTime;
+		this.descriptaion = descriptaion;
+		this.user = user;
+	}
+	public Task(String data, String startTime, String stopTime, String descriptaion) {
+		this.data = data;
+		this.startTime = startTime;
+		StopTime = stopTime;
+		this.descriptaion = descriptaion;
+	}
+	public Task() {
+		
+	}
+	
+
+}
